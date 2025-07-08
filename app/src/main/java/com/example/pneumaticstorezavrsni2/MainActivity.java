a6d9709dbde099109529414e1676d7bfdb0ad365
KolegaDzan <jkovacevic1@uniri.hr>
2025-07-09 01:38:48 +0200

    Second Commit

--- app/src/main/java/com/example/pneumaticstorezavrsni2/MainActivity.java
+++ app/src/main/java/com/example/pneumaticstorezavrsni2/MainActivity.java
@@ -3,22 +3,54 @@ package com.example.pneumaticstorezavrsni2;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.pneumaticstorezavrsni2.fragments.*;
+import com.example.pneumaticstorezavrsni2.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new StoreFragment())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_store) {
+                selectedFragment = new StoreFragment();
+            } else if (itemId == R.id.nav_library) {
+                selectedFragment = new LibraryFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            } else if (itemId == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
         });
     }
 }
